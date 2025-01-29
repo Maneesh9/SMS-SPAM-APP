187a055b737ee618959322bed51176290bdcc97c
Maneesh9 <amerpetamaneesh@gmail.com>
2025-01-29 23:49:02 +0530

    my project

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pickle
+
+model = pickle.load(open('C:/Users/amerp/OneDrive/Desktop/New folder/SMS_SPAM/spam.pkl','rb'))
+cv = pickle.load(open('C:/Users/amerp/OneDrive/Desktop/New folder/SMS_SPAM//vectorizer.pkl','rb'))
+
+
+st.title("EMAIL SPAM CLASSIFICATION APPLICATION")
+st.write("This is a Machine Learning application to classify emails as spam or ham.")
+user_input= st.text_area("Enter an email to classify",height=150)
+
+if st.button("Classify") :
+    if user_input:
+        data = [user_input]
+        vectorized_data = cv.transform(data).toarray()
+        result = model.predict(vectorized_data)
+        if result[0]==0:
+            st.write("The email is not spam")
+        else:
+            st.write("The email is spam")
+    else:
+        st.write("Please type Email to classify")
